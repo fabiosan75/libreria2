9fb04157db53066c20d9b779c823a001a53b9e4c
fabiosan75 <93453144+fabiosan75@users.noreply.github.com>
2022-02-16 22:46:31 -0500

    Crear Class2

---
+++ src/class2.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class2 Class Doc Comment
+ *
+ * @category Class
+ * @package  Libreria2
+ * @author   fabiosan75 <fabiosan75@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/fabiosan75
+ *
+ */
+
+ namespace src;
+
+/**
+ * Class2
+ */
+
+class Class2
+{
+}
+
+
+?>

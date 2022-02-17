66e00bb640f632b3f7e1b1e5128394a16c085c49
fabiosan75 <fabiosan75@gmail.com>
2022-02-17 17:59:06 -0500

    Actualizacion class2

--- src/class2.php
+++ src/class2.php
@@ -7,7 +7,7 @@
  * @package  Libreria2
  * @author   fabiosan75 <fabiosan75@gmail.com>
  * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     https://github.com/fabiosan75
+ * @link     https://github.com/fabiosan75/libreria2
  *
  */
 
